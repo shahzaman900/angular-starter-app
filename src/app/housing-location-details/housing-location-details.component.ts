eb4e6a2f281bc75fee8b8abf6dac02c93a6acf07
sheezu900 <shahzamanlive2@gmail.com>
2023-12-06 15:41:44 +0500

    add location details compomnent

---
+++ src/app/housing-location-details/housing-location-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-housing-location-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './housing-location-details.component.html',
+  styleUrl: './housing-location-details.component.css'
+})
+export class HousingLocationDetailsComponent {
+
+}
