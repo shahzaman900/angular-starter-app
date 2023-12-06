fb1bf9c0f6581c199364b8af6d4f6d2f41e93983
sheezu900 <shahzamanlive2@gmail.com>
2023-12-06 16:01:34 +0500

    add page details component route

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './app/home/home.component';
+import { HousingLocationDetailsComponent } from './app/housing-location-details/housing-location-details.component';
 
 const routeConfig: Routes = [
   {
     path: '',
     title: 'Home Page',
     component: HomeComponent
+  },
+  {
+    path: 'details',
+    title: 'Page Details',
+    component: HousingLocationDetailsComponent
   }
 ]
 
